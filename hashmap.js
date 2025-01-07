11de63550cd8b4d5011b2e1b7203c2b0395f5e31
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-01-07 19:46:21 +0530

    revamp time

--- hashmap.js
+++ hashmap.js
@@ -1,42 +1,34 @@
-/* if (index < 0 || index >= buckets.length) {
-  throw new Error("Trying to access index out of bounds");
-} */
-
-  const hashMap = (function(){
-	let capacity = 0;
-	let loadFactor = 0;
+const hashMap = (function(){
+	let capacity = 16;
+	let loadFactor = 0.75;
+	let length = 0;
 	const hashArray = [];
-	
+
 	//hashes input into corresponding output
 	function hash(key){
 		let code = 0;
 		const primeNumber = 17;
 		for(let i = 0;i < key.length;i++){
 			code = primeNumber * (code + key.charCodeAt(i));
-			code = code%16;
+			code = code%capacity;
 		}
 		return code;
 	}
-	
-	//check for key and insert key and value
+
 	function set(key,value){
-		const code = hash(key); //generate hash code from given key
-		if(hashArray[code] !== undefined){
-			
+		const index = hash(key);
+		if(hashArray[index] === undefined){
+			hashArray[index] = {key:key,value:value};
+			++length;
+		}
+		else{
+			hashArray[index] = {key:key,value:value};
 		}
 	}
-	
-	//return value at index or null 
-	function get(key){
-		
-	}
-	
-	//
-	//check for
 
 	return{
 		hash,
 	}
 })();
 
-console.log(hashMap.hash("Shineav"))
+console.log(hashMap.hash("shivane"))
